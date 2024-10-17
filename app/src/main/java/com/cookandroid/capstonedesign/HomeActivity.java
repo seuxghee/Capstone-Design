f4c4132228983d48889525480b3a40ed621a4983
yangseunghee <did030930@gmail.com>
2024-10-17 17:12:08 +0900

    홈 UI 구현 완료

--- app/src/main/java/com/cookandroid/capstonedesign/HomeActivity.java
+++ app/src/main/java/com/cookandroid/capstonedesign/HomeActivity.java
@@ -1,12 +1,59 @@
 package com.cookandroid.capstonedesign;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btn_AI;
+    Button btn_dic;
+    Button btn_map;
+    Button btn_myinfo;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn_AI = (Button) findViewById(R.id.btn_AI);
+        Button btn_dic = (Button) findViewById(R.id.btn_dic);
+        Button btn_map = (Button) findViewById(R.id.btn_map);
+        Button btn_myinfo = (Button) findViewById(R.id.btn_myinfo);
+
+        //AI카메라 액티비티로 이동
+        btn_AI.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+        //피부과 사전 액티비티로 이동
+        btn_dic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DictionaryActivity.class);
+                startActivity(intent);
+            }
+        });
+        //피부과 지도 액티비티로 이동
+        btn_map .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Mypage 액티비티로 이동
+        btn_myinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MyinfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cookandroid/capstonedesign/MyinfoActivity.java
@@ -0,0 +1,12 @@
+package com.cookandroid.capstonedesign;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyinfoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myinfo);
+    }
+}
