6640abc812cd444a0a6c30f0159937a3e75555c3
yangseunghee <did030930@gmail.com>
2024-10-17 16:39:22 +0900

    로그인 구현 화면 UI 구현 완료

--- app/src/main/java/com/cookandroid/capstonedesign/LoginActivity.java
+++ app/src/main/java/com/cookandroid/capstonedesign/LoginActivity.java
@@ -1,14 +1,46 @@
 package com.cookandroid.capstonedesign;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btn_login,btn_register ;
+    EditText et_email;
+    EditText et_pwd;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //로그인 구현 화면
+
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_pwd = (EditText) findViewById(R.id.et_pwd);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+       //로그인 버튼
+        Button btn_register = (Button) findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+       //회원가입 버튼
+        btn_register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
