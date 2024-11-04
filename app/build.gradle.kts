2321cc89cc21ee79921f39aa3e0ee36bd63e39bb
yangseunghee <did030930@gmail.com>
2024-11-04 17:39:32 +0900

    지도 부분 코드 옮기기(손륜)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,12 @@ android {
 }
 
 dependencies {
+    implementation ("com.naver.maps:map-sdk:3.18.0")
+    implementation ("androidx.appcompat:appcompat:1.2.0")
+    implementation ("com.google.android.material:material:1.2.1")
+    implementation ("androidx.activity:activity:1.2.2")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
 
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -42,6 +48,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.tensorflow.lite.support)
     implementation(libs.tensorflow.lite.metadata)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
